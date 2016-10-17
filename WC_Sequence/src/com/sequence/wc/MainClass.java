6b349f6e784f8ff7cddbce739dd2059836380cc7
kbajaj100 <kbajaj100@gmail.com>
2016-10-17 09:43:20 -0400

    Initial setup complete. 1. Able to connect to DB 2. Able to get the number of claims to be sequenced.

--- WC_Sequence/src/com/sequence/wc/MainClass.java
+++ WC_Sequence/src/com/sequence/wc/MainClass.java
@@ -1,10 +1,28 @@
 package com.sequence.wc;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException, SQLException {
 		// TODO Auto-generated method stub
 
+		/*
+		 * Get the Claim IDs in a table with Table ID identity
+		 * For each Claim ID, get the distinct list of DXs
+		 * Identify if there are any L or Ms
+		 * Arrange the rest alphabetically
+		 */
+		
+		int max;
+		
+		DBExecute myDB = new DBExecute();
+
+		max = myDB.getmax();
+	
+		System.out.println(max);
 	}
 
 }
